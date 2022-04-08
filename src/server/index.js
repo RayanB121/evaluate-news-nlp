36be07be2e567cf06ce4080da6521b9ad327e224
Rayan <rayan.bafageeh@gmail.com>
2022-04-08 04:58:35 +0300

    .

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,21 @@
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
-
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+const baseUrl='https://api.meaningcloud.com/sentiment-2.1';
+const apiKey= process.env.API_KEY;
+let userInput=[];
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -20,4 +29,12 @@ app.listen(8080, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.post('/input',async(req,res)=>{
+    userInput= req.body.url;
+    const apiUrl=`${baseUrl}key=${apiKey}&url=${userInput}&lang=en`;
+    const response= await fetch(apiUrl);
+    const data=await response.json();
+    res.send(data);
 })
